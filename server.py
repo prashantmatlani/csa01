8eba78dd3f2793c053ba139cf247b9eaba1c8b03
Prashant Matlani <prashantmatlani@gmail.com>
2026-04-09 15:37:38 +0000

    removed server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-
-#from server.app import main
-import uvicorn
-#import server.app
-from server.app import app, main  # import both app object and main()
-
-if __name__ == "__main__":
-    #main()
-    uvicorn.run("server.app:app", host="127.0.0.1", port=8000, reload=True)
